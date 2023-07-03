8e5e0da30e71d504b6e2c755bfb44432171965fc
fshippman <fshippman@yahoo.com>
2023-07-03 20:33:11 +0200

    final

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let basket = [];
 
 let foods = [{
     'name': 'Pizza Margherita',
-    'description': 'mit Käse, Tomatensauce und Schokolade',
+    'description': 'mit Käse und Tomatensauce und Schokolade',
     'choice': 'Wahl aus: Klein, Ø 24cm, Normal, Ø 29cm oder Familia, Ø 40cm',
     'price': '6.80',
 },
@@ -65,7 +65,6 @@ function show() {
 function createShowHTML(food) {
     const name = food['name'];
     const price = food['price'];
-
     return /*html*/`
     <div class="card" onclick="addToBasket('${name}', ${price});">
         <div class = "card-content">
@@ -82,15 +81,15 @@ function createShowHTML(food) {
 //--------------------------------BASKET--------------------------------
 
 function addToBasket(name, price) {
-    let index = basket.findIndex(pizza => {
-        return pizza.name === name;
+    let index = basket.findIndex(basketelement => {
+        return basketelement.name === name;
     })
     if (index === -1) {
         basket.push({
             'counter': 1,
             'name': name,
             'price': price
-        });
+        }); 
     } else {
         basket[index]['counter']++;
     }
@@ -125,7 +124,6 @@ function updateShoppingBasket() {
 function updateShoppingBasketByIds(basketId, basketPricesId, defaultBasketId) {
     let basketDishes = document.getElementById(basketId);
     let basketPrices = document.getElementById(basketPricesId);
-
     basketDishes.innerHTML = '';
     if (basket.length < 1) {
         basketPrices.innerHTML = '';
@@ -169,7 +167,7 @@ function createBasketHTML(basket, i) {
 }
 
 
-function generateCostOverviewHTML(sum, finalSum){
+function generateCostOverviewHTML(sum, finalSum) {
     return /*html*/`
         <div class="space-between">
             <div>Zwischensumme</div>
@@ -200,13 +198,11 @@ function hideEmptyBasket(defaultBasketId) {
 
 function createResponsiveBasketSection() {
     let responsiveSection = document.getElementById("responsive-basket-section");
-
     if (basket.length === 0) {
         responsiveSection.classList.add('d-none');
         responsiveSection.innerHTML = '';
         return;
     }
-
     responsiveSection.classList.remove('d-none');
     let sum = 0;
     for (let dish of basket) {
@@ -230,15 +226,17 @@ function renderResponsiveButton(finalSum) {
                >
                     <path d="M4.559 7l4.701-4.702c.198-.198.459-.298.72-.298.613 0 1.02.505 1.02 1.029 0 .25-.092.504-.299.711l-3.26 3.26h-2.882zm12 0h2.883l-4.702-4.702c-.198-.198-.459-.298-.72-.298-.613 0-1.02.505-1.02 1.029 0 .25.092.504.299.711l3.26 3.26zm3.703 4l-.016.041-3.598 8.959h-9.296l-3.597-8.961-.016-.039h16.523zm3.738-2h-24v2h.643c.535 0 1.021.304 1.256.784l4.101 10.216h12l4.102-10.214c.234-.481.722-.786 1.256-.786h.642v-2zm-14 5c0-.552-.447-1-1-1s-1 .448-1 1v3c0 .552.447 1 1 1s1-.448 1-1v-3zm3 0c0-.552-.447-1-1-1s-1 .448-1 1v3c0 .552.447 1 1 1s1-.448 1-1v-3zm3 0c0-.552-.447-1-1-1s-1 .448-1 1v3c0 .552.447 1 1 1s1-.448 1-1v-3z"/>
                </svg>
-               <span>Warenkorb</span><div> (${finalSum}) €</div>
+               <span class="responsive-button-span">Warenkorb</span><div> (${finalSum}) €</div>
            </div>
        </button>
    `;
 }
 
+
 function openBasketFullscreen() {
     document.getElementById("dialog").classList.remove('d-none');
 }
+
 
 function closeFullscreen() {
     document.getElementById("dialog").classList.add('d-none');
